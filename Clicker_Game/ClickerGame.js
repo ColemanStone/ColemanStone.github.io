01cc6624e6ca0de327200fc0c9022ac49a50c540
coleman <Stonecoleman4563@gmail.com>
2024-02-28 21:31:02 -0500

    Added some code to see if the button gets pressed and if the score is suppost to change

--- Clicker_Game/ClickerGame.js
+++ Clicker_Game/ClickerGame.js
@@ -3,7 +3,7 @@ const button = document.getElementById('clicker-button');
 const scoreElement = document.getElementById('clicker-counter');
 
 button.addEventListener('click', () => {
-    console.log('Button was clicked');
+    console.log('Button was clicked!');
     score++;
     scoreElement.innerText = score;
     console.log('Score was updated to', score);
