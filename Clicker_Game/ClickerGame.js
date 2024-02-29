99fdf1b74e85bcdd9b8388272c90536360bd498b
coleman <Stonecoleman4563@gmail.com>
2024-02-28 21:30:07 -0500

    Mabye fixed the score in the clicker game

--- Clicker_Game/ClickerGame.js
+++ Clicker_Game/ClickerGame.js
@@ -3,6 +3,8 @@ const button = document.getElementById('clicker-button');
 const scoreElement = document.getElementById('clicker-counter');
 
 button.addEventListener('click', () => {
+    console.log('Button was clicked');
     score++;
     scoreElement.innerText = score;
+    console.log('Score was updated to', score);
 });
