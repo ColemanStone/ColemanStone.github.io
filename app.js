1a92564a4a9fbd8962b35c5e422d4cb35b3efc95
Coleman Stone <stonecoleman4563@gmail.com>
2025-05-03 11:14:50 -0400

    Fixed css file location code

--- app.js
+++ app.js
@@ -16,11 +16,6 @@ app.get('/register', (req, res) => res.render('register'));
 app.get('/project-home', (req, res) => res.render('project_home_page'));
 app.get('/my-info', (req, res) => res.render('my_information'));
 
-// Custom event pages
-app.get('/2025-reunion/boating', (req, res) => res.render('2025_morgan_reunion_boating'));
-app.get('/2025-reunion/flower-picking', (req, res) => res.render('2025_morgan_reunion_flower_picking'));
-app.get('/2025-reunion/golf', (req, res) => res.render('2025_morgan_reunion_golf'));
-
 // Start server
 app.listen(port, () => {
     console.log(`Site running at http://localhost:${port}`);
